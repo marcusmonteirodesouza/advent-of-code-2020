c0916c77d425ac90bbd06be624b565adfb66cafe
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2020-12-28 01:32:03 -0500

    Add day solution part 2

--- src/day-1/day-1.js
+++ src/day-1/day-1.js
@@ -1,33 +1,72 @@
 const fs = require('fs')
 const path = require('path')
 
-const solution = () =>
+const readEntries = () =>
   new Promise((resolve, reject) => {
     fs.readFile(path.join(__dirname, 'input'), 'utf8', (err, data) => {
       if (err) {
         reject(err)
       }
 
-      const sumTo = 2020
-
-      const entries = data
-        .trim()
-        .split('\n')
-        .map((entry) => Number.parseInt(entry, 10))
-
-      entries.forEach((entry) => {
-        const i = entries.indexOf(sumTo - entry)
-        if (i !== -1) {
-          resolve(entry * entries[i])
-        }
-      })
-
-      reject(
-        new Error(
-          `Could not find two numbers that sum to ${sumTo} in ${entries}`
-        )
+      resolve(
+        data
+          .trim()
+          .split('\n')
+          .map((entry) => Number.parseInt(entry, 10))
       )
     })
   })
 
-module.exports = solution
+const solutionPartOne = async () => {
+  const sumTo = 2020
+  const entries = await readEntries()
+
+  for (let i = 0; i < entries.length; i += 1) {
+    const entry = entries[i]
+    const j = entries.indexOf(sumTo - entry)
+    if (j !== -1) {
+      return entry * entries[j]
+    }
+  }
+
+  throw new Error(
+    `Could not find two numbers that sum to ${sumTo} in ${entries}`
+  )
+}
+
+const solutionPartTwo = async () => {
+  const sumTo = 2020
+  const entries = await readEntries()
+
+  entries.sort((a, b) => a - b)
+
+  for (let i = 0; i < entries.length; i += 1) {
+    let left = i + 1
+    let right = entries.length - 1
+
+    while (left < right) {
+      const a = entries[i]
+      const b = entries[left]
+      const c = entries[right]
+      const sum = a + b + c
+
+      if (sum === sumTo) {
+        return a * b * c
+      }
+      if (sum < sumTo) {
+        left += 1
+      } else {
+        right -= 1
+      }
+    }
+  }
+
+  throw new Error(
+    `Could not find two numbers that sum to ${sumTo} in ${entries}`
+  )
+}
+
+module.exports = {
+  solutionPartOne,
+  solutionPartTwo,
+}
--- src/day-1/day-1.test.js
+++ src/day-1/day-1.test.js
@@ -1,9 +1,15 @@
-const solution = require('./day-1.js')
+const { solutionPartOne, solutionPartTwo } = require('./day-1.js')
 
 describe('Day 1', () => {
-  describe('solution', () => {
+  describe('solutionPartOne', () => {
     test('Find the two entries that sum to 2020; what do you get if you multiply them together?', async () => {
-      await expect(solution()).resolves.toBe(494475)
+      await expect(solutionPartOne()).resolves.toBe(494475)
+    })
+  })
+
+  describe('solutionPartTwo', () => {
+    test('what is the product of the three entries that sum to 2020?', async () => {
+      await expect(solutionPartTwo()).resolves.toBe(267520550)
     })
   })
 })
